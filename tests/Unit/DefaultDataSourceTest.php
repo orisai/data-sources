1e06b6278002bc33ab991b281974b73589b5a111
Marek Bartoš <bartos.developer152@gmail.com>
2020-12-10 16:11:33 +0100

    DefaultDataSource test

---
+++ tests/Unit/DefaultDataSourceTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Orisai\DataSources\Unit;
+
+use Orisai\DataSources\DefaultDataSource;
+use Orisai\Exceptions\Logic\InvalidState;
+use PHPStan\Testing\TestCase;
+use Tests\Orisai\DataSources\Doubles\SerializeEncoder;
+
+final class DefaultDataSourceTest extends TestCase
+{
+
+	public function testNoEncoder(): void
+	{
+		$encoders = [
+			new SerializeEncoder(),
+		];
+		$source = new DefaultDataSource($encoders);
+
+		$this->expectException(InvalidState::class);
+		$this->expectExceptionMessage('No encoder is available for file type neon.');
+
+		$source->toContent(['foo' => 'bar'], 'neon');
+	}
+
+}
