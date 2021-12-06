a6e34b2b6b6a66a19c35e2578f6e62304d0b5268
Marek Bartoš <bartos.developer152@gmail.com>
2021-12-06 20:49:04 +0100

    Tests: fix PHP version check

--- tests/Unit/Bridge/NetteDI/DataSourceExtensionTest.php
+++ tests/Unit/Bridge/NetteDI/DataSourceExtensionTest.php
@@ -31,7 +31,7 @@ final class DataSourceExtensionTest extends TestCase
 		parent::setUp();
 
 		$this->rootDir = dirname(__DIR__, 4);
-		if (PHP_VERSION_ID < 81_000) {
+		if (PHP_VERSION_ID < 8_01_00) {
 			@mkdir("$this->rootDir/var/build");
 		}
 	}
